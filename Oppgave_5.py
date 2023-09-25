a40a57aa143e21f1c713f9dee20c9d50e94a580e
RinkyDinky-for-real <134487126+RinkyDinky-for-real@users.noreply.github.com>
2023-09-25 14:36:05 +0200

    glubb

--- Oppgave_5.py
+++ Oppgave_5.py
@@ -11,6 +11,6 @@ while len(player_score) < player_antall:
 
 for n in range(len(player_score)):
     for i in range(len(dart_antall)):
-        dart_antall[i] = random.randrange(0,60)
+        dart_antall[i] = random.randrange(0,61)
     player_score[n] = dart_antall[0]+dart_antall[1]+ dart_antall[2]
     print (f"Den totale poengsummen for spiller {n+1} ble: {player_score[n]}, med kastene: {dart_antall[0]},{dart_antall[1]},{dart_antall[2]}.")
