021fbd776d3e216c9f1427d2e525961a6e80a482
Emil Lundemo Aagnes <emilaagnes@gmail.com>
2023-10-27 14:58:53 +0200

    mehre

--- Oppgave2.py
+++ Oppgave2.py
@@ -22,22 +22,79 @@ class Deck:
             for rank in ranks:
                 self.cards.append(Card(rank, suit))
 class Hand:
-    def __init__(self):
-        self.hands = ()
-        pass
-# Creating a deck, shuffling it, and drawing a card
-idealdeck = Deck()
-playdeck = list(idealdeck.cards)
-player = Hand()
-dealer = Hand()
+    def __init__(self, x):
+        self.name = x
+        self.hands = []
+        self.value = int(0)
+        self.has_ace = False
+values = {
+    'Ace': 1,  # Ess håndteres inni funksjonen
+    'Two': 2,
+    'Three': 3,
+    'Four': 4,
+    'Five': 5,
+    'Six': 6,
+    'Seven': 7,
+    'Eight': 8,
+    'Nine': 9,
+    'Ten': 10,
+    'Jack': 10,
+    'Queen': 10,
+    'King': 10
+}
+def rank_value(rank):
+    return values[rank]
 def draw_card(x):
     if x:
         return x.pop()
     else:
         return None
-card = draw_card(playdeck)
-print(f"Drawn card: {card.rank} of {card.suit}")
-playdeck = list(idealdeck.cards)
-for card in playdeck:
-    print(f"{card}: {card.rank} of {card.suit}")
+def draw_to_hand(x, y):
+    if x:
+        y.hands.append(x)
+def sum_hand(x):
+    for card in x.hands:
+        x.value += rank_value(card.rank)
+        if card.rank == "Ace":
+            x.has_ace = True
+    if x.has_ace and x.value <= 11: #sjekker om ess må være 11 eller 1
+        x.value += 10  
+    return x.value
+def sum_hand_dealer(x):
+    d_card = x.hands[0]
+    if d_card:
+        x.value += rank_value(d_card.rank)
+        if d_card.rank == "Ace":
+            x.has_ace = True
+    if x.has_ace and x.value <= 11: #sjekker om ess må være 11 eller 1
+        x.value += 10  
+    return x.value
+def showhand(x):
+    print(f"The {x.name}'s hand consists of the following cards:")
+    for card in x.hands:
+        print(f"{card.rank} of {card.suit}")
+    print(f"For a combined value of:{sum_hand(x)}")
+def showhand_dealer(x):
+    print(f"The {x.name}'s visible card is:")
+    d_card = x.hands[0]
+    if d_card:
+        print(f"{d_card.rank} of {d_card.suit}")
+    print(f"For a value of:{sum_hand_dealer(x)}")
+chips = 7
+wager = 0
+gameOver = False
 
+idealdeck = Deck()
+playdeck = list(idealdeck.cards)
+random.shuffle(playdeck)
+player = Hand("player")
+dealer = Hand("dealer")
+
+draw_to_hand(draw_card(playdeck), player)
+draw_to_hand(draw_card(playdeck), player)
+draw_to_hand(draw_card(playdeck), dealer)
+draw_to_hand(draw_card(playdeck), dealer)
+showhand(player)
+showhand_dealer(dealer)
+#while gameOver == False:#gameloop
+#    print()
